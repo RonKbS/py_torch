b6f356df15cd27e988cf61f7d3a3483dc5268441
RonKbS <stephen.bamwite@gmail.com>
2019-09-08 10:17:48 +0300

    flattening images

---
+++ handrwitten_digits.py
@@ -0,0 +1,48 @@
+# part 2: neural networks in pytorch
+
+
+import numpy as np
+import torch
+from torchvision import datasets, transforms
+# import helper
+
+import matplotlib.pyplot as plt
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5,), (0.5,)),
+                              ])
+
+# Download and load the training data
+trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+# print(type(images))
+# print(images.shape)
+# print(labels.shape)
+
+# plt.imshow(images[1].numpy().squeeze(), cmap='Greys_r')
+# plt.show()
+
+# build a simple network for this dataset using weight matrices and matrix
+
+
+## Solution
+def activation(x):
+    return 1/(1+torch.exp(-x))
+
+# Flatten the input images
+inputs = images.view(images.shape[0], -1)
+
+# Create parameters
+w1 = torch.randn(784, 256)
+b1 = torch.randn(256)
+
+w2 = torch.randn(256, 10)
+b2 = torch.randn(10)
+
+h = activation(torch.mm(inputs, w1) + b1)
+
+out = torch.mm(h, w2) + b2
+print(len(out))
